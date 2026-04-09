d966b18281611db85d93ef9fe50587a47ad7988d
Cmvng <cmvceo1000@gmail.com>
2026-04-09 13:07:37 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Feed from './pages/feed'
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/feed" element={<Feed />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  const path = window.location.pathname
+  if (path === '/feed') return <Feed />
+  return <Home />
 }
