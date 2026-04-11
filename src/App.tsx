a79d69d2d1fdec24232ec7c81f931ed54a462462
Cmvng <cmvceo1000@gmail.com>
2026-04-11 15:11:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from './pages/home'
 import Feed from './pages/feed'
 import TierList from './pages/tierlist'
+import Admin from './pages/admin'
 
 export default function App() {
   const path = window.location.pathname
   if (path === '/feed') return <Feed />
   if (path === '/tierlist') return <TierList />
+  if (path === '/admin') return <Admin />
   return <Home />
 }
